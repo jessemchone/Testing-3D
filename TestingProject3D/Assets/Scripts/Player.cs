210b93c585707072a6c4ca1e21e941d221ed9344
jessemchone <50703311+jessemchone@users.noreply.github.com>
2021-01-28 21:50:07 +0900

    Random practice

--- TestingProject3D/Assets/Scripts/Player.cs
+++ TestingProject3D/Assets/Scripts/Player.cs
@@ -6,12 +6,41 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Spell[] _spells;
 
-    public int playerEXP;
-    
+    [SerializeField] string[] _names = new string[] { "Poison", "Fire", "Water", "Ice"};
+
+    private int playerEXP;
+
+    public static Player instance;
+
+    public int PlayerEXP
+    {
+        get
+        {
+            return playerEXP;
+        }
+        private set
+        {
+            playerEXP = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < _names.Length; i++)
+        {
+            Debug.Log(_names[i]);
+        }
     }
 
     // Update is called once per frame
---
+++ TestingProject3D/Assets/Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private Player player;
+
+    private int exp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        exp = player.PlayerEXP;
+        Debug.Log("Test exp is " + exp);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            exp = player.PlayerEXP;
+            Debug.Log("Test exp is " + exp);
+        }
+    }
+}
